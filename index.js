80f710e43d45689208002496f2a35da127192ddc
bharathkumar-turing <bharath.b@turing.com>
2026-04-17 16:45:31 +0530

    feat: helm check commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Platform Engineering New cicd release! hohoooo🚀');
+  res.send('Hello from Platform Engineering New cicd release! hohoooo, helm added update🚀');
 });
 
 app.listen(3000, () => {
